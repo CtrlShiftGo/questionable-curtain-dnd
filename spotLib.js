9c5ed5334c40a84684de9baebb1164230d5fdbe1
Josh Wu <ctrlshiftgo@gmail.com>
2019-02-23 15:56:51 -0800

    Ådd initial function stubs

---
+++ spotLib.js
@@ -0,0 +1,19 @@
+export class SpotLib {
+    constructor() {
+        // Check if authentication tokens exist in storage
+        // Else get new auth and store
+
+    }
+
+    playSongs(songList) {
+        // TODO implement
+    }
+    
+    // Search for songs by name
+    // Returns: List of song IDs and Song Names
+    getSongsByName(name) {
+        // TODO implement
+    }
+
+    
+}
